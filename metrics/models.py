9b0b2278e1f288e03459f332a58b87bd0cc16d93
ccatalfo <foxnorth@gmail.com>
2014-04-04 20:57:36 -0400

    Initial import of basic models.

--- metrics/models.py
+++ metrics/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class MetricsSourceType(models.Model):
+    name = models.CharField(max_length=256)
+
+class MetricsSource(models.Model):
+    source_type = models.ForeignKey(MetricsSourceType)
+    name = models.CharField(max_length=256)
+
+class Metric(models.Model):
+    source = models.ForeignKey(MetricsSource)
+    time = models.DateTimeField(auto_now_add=True)
+    value = models.FloatField()
